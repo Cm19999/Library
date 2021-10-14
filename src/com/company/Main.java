c1ad74d97a01dcbfbacac7694163679f1c084fe3
callum <cmarsden4163@googlemail.com>
2021-10-14 17:23:10 +0100

    testing branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+
+
         Journal p1 = new Journal("DVD");
         p1.use();
         p1.store();
